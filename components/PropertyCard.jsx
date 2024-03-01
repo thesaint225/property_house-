5f155f43385bf03563cb4bc773ae7b86d36c9132
henrymiessan <miessanhenri@gmail.com>
2024-03-01 07:28:09 +0000

    fixed bugs around the HomeProperties

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -21,41 +21,41 @@ const PropertyCard = ({ property }) => {
     }
   };
   return (
-    // <!-- Listing 1 -->
-    <div class="rounded-xl shadow-md relative">
+    <div className="bg-white rounded-xl shadow-md relative">
       <Image
         src={`/images/properties/${property.images[0]}`}
         alt=""
         height={0}
         width={0}
-        sizes="100vw"
-        class="w-full h-auto rounded-t-xl"
+        sizes="100 vw"
+        className="w-full h-auto rounded-t-xl"
       />
-      <div class="p-4">
-        <div class="text-left md:text-center lg:text-left mb-6">
-          <div class="text-gray-600">{property.type}</div>
-          <h3 class="text-xl font-bold">{property.name}</h3>
+      <div className="p-4">
+        <div className="text-left md:text-center lg:text-left mb-6">
+          <div className="text-gray-600">{property.type}</div>
+          <h3 className="text-xl font-bold">{property.name}</h3>
         </div>
-        <h3 class="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
+        <h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
           ${getRateDisplay()}
         </h3>
 
-        <div class="flex justify-center gap-4 text-gray-500 mb-4">
+        <div className="flex justify-center gap-4 text-gray-500 mb-4">
           <p>
             <FaBed className="inline mr-2" /> {property.beds}{" "}
-            <span class="md:hidden lg:inline">Beds</span>
+            <span className="md:hidden lg:inline">Beds</span>
           </p>
           <p>
             <FaBath className="inline mr-2" /> {property.baths}{" "}
-            <span class="md:hidden lg:inline">Baths</span>
+            <span className="md:hidden lg:inline">Baths</span>
           </p>
           <p>
             <FaRulerCombined className="inline mr-2" />
-            {property.square_feet} <span class="md:hidden lg:inline">sqft</span>
+            {property.square_feet}{" "}
+            <span className="md:hidden lg:inline">sqft</span>
           </p>
         </div>
 
-        <div class="flex justify-center gap-4 text-green-900 text-sm mb-4">
+        <div className="flex justify-center gap-4 text-green-900 text-sm mb-4">
           {property.rates.nightly && (
             <p>
               <FaMoneyBill className="inline mr-2" /> Nightly
@@ -64,29 +64,28 @@ const PropertyCard = ({ property }) => {
 
           {property.rates.weekly && (
             <p>
-              <FaMoneyBill className="inline mr-2" /> Weekly
+              <FaMoneyBill className="inline mr-2" /> weekly
             </p>
           )}
           {property.rates.monthly && (
             <p>
-              <FaMoneyBill className="inline mr-2" /> Nightly
+              <FaMoneyBill className="inline mr-2" /> monthly
             </p>
           )}
         </div>
 
-        <div class="border border-gray-100 mb-5"></div>
+        <div className="border border-gray-100 mb-5"></div>
 
-        <div class="flex flex-col lg:flex-row justify-between mb-4">
-          <div class="flex align-middle gap-2 mb-4 lg:mb-0">
-            <FaMapMarker className="inline mr-2" />
-            <span class="text-orange-700">
-              {" "}
-              {property.location.city} {property.location.state} {""}{" "}
+        <div className="flex flex-col lg:flex-row justify-between mb-4">
+          <div className="flex align-middle gap-2 mb-4 lg:mb-0">
+            <FaMapMarker className="text-orange-700 mt-1" />
+            <span className="text-orange-700">
+              {property.location.city} {property.location.state}{" "}
             </span>
           </div>
           <Link
             href={`/properties/${property._id}`}
-            class="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+            className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Details
           </Link>
