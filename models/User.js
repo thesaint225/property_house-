01bd8eb8738526cd483842c09be2ed3c50f49769
henrymiessan <miessanhenri@gmail.com>
2024-03-04 13:35:20 +0000

    models added

---
+++ models/User.js
@@ -0,0 +1,32 @@
+import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: [true, "Email already exist "],
+      required: [true, "Email is require "],
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+    },
+
+    image: {
+      type: String,
+    },
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "property",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = models.User || model("User", userSchema);
+
+export default User;
