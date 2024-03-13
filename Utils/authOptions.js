d6d763083fd95ee700c872a9eb1ad2b078212ec1
henrymiessan <miessanhenri@gmail.com>
2024-03-13 18:33:48 +0000

    sign Out & Protect Routes

--- Utils/authOptions.js
+++ Utils/authOptions.js
@@ -1,3 +1,5 @@
+import connectedDB from "@/config/database";
+import User from "@/models/User";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
@@ -18,15 +20,31 @@ export const authOptions = {
     // Invoked on sucessful signin
     async signIn({ profile }) {
       // 1.connect to the database
+      await connectedDB();
       // 2.Check if user exists
+      const userExists = await User.findOne({ email: profile.email });
       // 3.If not ,then add user to database
+      if (!userExists) {
+        // truncate user name if too long
+        const username = profile.name.slice(0, 20);
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
       // 4.return true to allow sign in
+      return true;
     },
     // modifies the session object
     async session({ session }) {
       // Get the user from database
+      const user = await User.findOne({ email: session.user.email });
       // Assign the user id to the session
+      session.user.id = user._id.toString();
       // return session
+      return session;
     },
   },
 };
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,6 +10,7 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Navbar = () => {
   const { data: session } = useSession();
+  const profileImage = session?.user?.image;
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
   const [providers, setProviders] = useState(null);
@@ -24,7 +25,9 @@ const Navbar = () => {
     setAuthProviders();
   }, []);
 
-  console.log(providers);
+  console.log(profileImage);
+
+  // console.log(providers);
 
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
@@ -167,8 +170,10 @@ const Navbar = () => {
                     <span className="sr-only">Open user menu</span>
                     <Image
                       className="h-8 w-8 rounded-full"
-                      src={profileDefault}
+                      src={profileImage || profileDefault}
                       alt=""
+                      width={40}
+                      height={40}
                     />
                   </button>
                 </div>
@@ -189,6 +194,7 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-0"
+                      onClick={() => setIsProfileMenuOpen(false)}
                     >
                       Your Profile
                     </Link>
@@ -198,11 +204,16 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-2"
+                      onClick={() => setIsProfileMenuOpen(false)}
                     >
                       Saved Properties
                     </Link>
+
                     <button
-                      href="#"
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                        signOut();
+                      }}
                       className="block px-4 py-2 text-sm text-gray-700"
                       role="menuitem"
                       tabIndex="-1"
