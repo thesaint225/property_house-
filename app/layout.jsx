c1f7c575841afbec828f80c921c8e1694cd40a0b
henrymiessan <miessanhenri@gmail.com>
2024-04-05 09:54:59 +0000

    react-toastify added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import Navbar from "@/components/Navbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "@/assets/styles/global.css";
 import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
@@ -17,6 +19,7 @@ const MainLayout = ({ children }) => {
           <Navbar />
           <main>{children} </main>
           <Footer />
+          <ToastContainer />
         </body>
       </html>
     </AuthProvider>
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { useSession } from "next-auth/react";
 import profileDefault from "@/assets/styles/images/profile.png";
 import Spinner from "@/components/Spinner";
+import { toast } from "react-toastify";
 
 const ProfilePage = () => {
   const { data: session } = useSession();
@@ -56,13 +57,13 @@ const ProfilePage = () => {
           (property) => property._id !== propertyId
         );
         setProperties(updatedProperties);
-        alert("Property Deleted");
+        toast.success("Property Deleted");
       } else {
-        alert("Failed to delete property");
+        toast.error("Failed to delete property");
       }
     } catch (error) {
       console.log(error);
-      alert("Failed to delete property");
+      toast.error("Failed to delete property");
     }
   };
   return (
